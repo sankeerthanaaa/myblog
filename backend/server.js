078f124f86749ba16a2de8b8da9116159e01d46a
sankeerthanaaa <kuntasankeerthana246@gmail.com>
2026-04-18 15:51:19 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: [
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://myblog-five-nu.vercel.app"
+    "https://myblog-ffnxizg7e-sankeerthanaaas-projects.vercel.app"
   ],
   credentials: true
 }));// body parser
